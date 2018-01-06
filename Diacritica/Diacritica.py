a1ebadb8e66d3df42fbcd26b9433ec7254b47ad6
matei <matei@telecinemateca>
2018-01-06 18:32:10 +0200

    new commit

--- Diacritica/Diacritica.py
+++ Diacritica/Diacritica.py
@@ -31,19 +31,15 @@ sys.setdefaultencoding('iso8859-2')
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', filename='/home/matei/Documents/Python/Diacritica/Diacritica.log', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
 
-
 reps = {u'\u0103':'a', u'\u0102':'A', u'\u00E2':'a', u'\u00C2':'A', u'\u00EE':'i', u'\u00CE':'I', u'\u015F':'s', u'\u015E':'S', u'\u0163':'t', u'\u0162':'T'}
-
 
 # seteaza fisiere subtitrare IN
 fileIn  = '/media/MoviesNSA310/incoming/Blade.Runner.2049.2017.1080p.BluRay.x264-SPARKS/Blade.Runner.2049.2017.1080p.BluRay.x264-SPARKS.Ro.srt'
-
 
 fileOut = mydef.set_lang(fileIn)
 fileData = mydef.read_file(fileIn)
 newData = mydef.replace_all(fileData, reps)
 mydef.write_file(fileOut, newData)
-
 
 logging.info('Succes! S-a generat fisierul: {0}'.format(fileOut))
 print '\nSucces! S-a generat fisierul:', fileOut
